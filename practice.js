9cd382cddc9558d96a1e7b65a52184e7674ea8ce
Anand Pradeep <anandpradeep181@gmail.com>
2026-01-06 20:44:44 +0530

    jan 6

--- practice.js
+++ practice.js
@@ -14,3 +14,44 @@ for(let i=0;i<zero;i++){
     result.push(0)
 }
 console.log(result)
+
+
+let obj={
+    name:"ANAND",
+    age:18
+}
+obj.country="INDIA"
+console.log(obj)
+
+
+const obj={
+    name:"ANAND",
+    age:18,
+    country:"INDIA"
+}
+const keyArr=Object.keys(obj)
+console.log(keyArr)
+
+const obj={
+    name:"ANAND",
+    age:18,
+    address:{
+        city:"palakkad",
+        country:"india"
+    }
+}
+const clone=structuredClone(obj)
+clone.address.city="Kochi"
+
+console.log(obj.address.city)
+
+
+const arr=[1,2,3,4,5,3,7,1,3]
+let freq={}
+
+for(let item of arr){
+    freq[item]=(freq[item]||0)+1
+}
+console.log(freq)
+
+
